91e6ced4b5a9c764b1cebbfd809eb4177ce82f80
info@avalanche.lk <avalanche@bitbucket>
2019-03-27 18:51:59 +0530

    first commit

---
+++ app/src/main/java/lk/janiru/consumer/greentrack/db/AppDatabase.java
@@ -0,0 +1,12 @@
+package lk.janiru.consumer.greentrack.db;
+
+/*
+ *
+ * Project Name : ${PROJECT}
+ * Created by Janiru on 3/27/2019 1:43 PM.
+ *
+ */
+
+public class AppDatabase {
+
+}
---
+++ app/src/main/java/lk/janiru/consumer/greentrack/services/FirebaseService.java
@@ -0,0 +1,12 @@
+package lk.janiru.consumer.greentrack.services;
+
+/*
+ *
+ * Project Name : ${PROJECT}
+ * Created by Janiru on 3/27/2019 1:45 PM.
+ *
+ */
+
+public class FirebaseService {
+
+}
